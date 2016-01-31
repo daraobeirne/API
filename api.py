8062fbee26f0fc0f73e60855009a047c4189d78b
daraobeirne <daraobeirne@gmail.com>
2016-01-30 22:37:02 -0800

    done for the night

--- api.py
+++ api.py
@@ -16,6 +16,41 @@ class Human(object):
 		return "8 hours"
 """
 
+class CPIData(object):
+	"""Abstraction of the CPI data provided by FRED.
+	
+	This stores internally only one value per year.
+	
+	"""
+	
+	def __init__(self):
+		self.year_cpi = {}
+		self.last_year = None
+		self.first_year = None
+		
+	def load_from_url(self, url, save_as_file=None):
+		"""Loads data from a given url.
+		
+		The downloaded file can also be saved into a location for later
+		re-use with the "save_as_file" parameter specifying a filename.
+		
+		After fetching the file this implementation uses load_from_file
+		internally. 
+		
+		"""
+	
+	def load_from_file(self, fp):
+		"""Loads CPI data from a given file-like object."""
+		
+	def get_adjusted_price(self, price, year, current_year=None):
+		"""Returns the adapted price from a given year compared to what current
+		year has been specified.
+		
+		"""
+		
+		
+
+
 def main():
 	"""This function handles the actual logic of this script"""
 	
