e9dc88f7ad99daeb6adf3ef69d93edc9c8c47bac
daraobeirne <daraobeirne@gmail.com>
2016-01-30 22:23:15 -0800

    first commit for API workspace

---
+++ api.py
@@ -0,0 +1,42 @@
+from __future__ import print_function
+import requests
+
+
+CPI_DATA_URL = 'http://research.stlouisfed.org/fred2/data/CPIAUCSL.txt'
+
+
+
+
+"""
+class Human(object):
+	def __init__(self, name, birthday):
+		self.name = name
+		self.birthday = birthday
+	def get_sleep_time(self):
+		return "8 hours"
+"""
+
+def main():
+	"""This function handles the actual logic of this script"""
+	
+	# Grab CPI/Inflation data.
+	
+	#Grab API/game platform data.
+	
+	
+	
+	# Figure out the current price of each platform.
+	# This will require looping through each platform we received.
+	# calculate the adjusted price based on the CPI data we also received
+	# During this point, we should also validate our data so we do not skew
+	# our results.
+	
+	
+	#Gnerate a plot/bar graph for the adjusted price data. 
+	
+	
+	#Generate a CSV file to save for the adjusted price data. 
+	
+	
+	
+	
